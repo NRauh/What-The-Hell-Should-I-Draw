4d152ebe4712763eea97e0bdc56ee98718590f32
Nate Rauh <NRauh@users.noreply.github.com>
2014-07-06 18:47:25 -0500

    Allowed CORS

--- app.js
+++ app.js
@@ -25,6 +25,16 @@ var WordController = require("./WordController");
 	Adding body-parser middleware
 ***/
 app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+var allowCrossDomain = function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+
+	next();
+};
+app.use(allowCrossDomain);
 
 
 /***
