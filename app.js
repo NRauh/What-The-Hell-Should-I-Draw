deae9d9e75ba785bd1d8ec2775c350d0abf11ff2
Nate Rauh <NRauh@users.noreply.github.com>
2016-08-12 05:26:43 -0500

    Updated app.js
    Added some ES6.
    Repositioned some things.
    Changed how routes work to reflect changes to controller.
    Also changed routes to be /api/*subject

--- app.js
+++ app.js
@@ -1,56 +1,52 @@
-var express = require("express");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const SubjectController = require("./SubjectController");
+var db = mongoose.connection;
 var app = express();
-var bodyParser = require("body-parser");
 
 
 /***
 	Connecting to database
 ***/
-var mongoose = require("mongoose");
-var db = mongoose.connection;
 mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/subject");
 db.on("error", console.error.bind(console, "Error:"));
-db.once("open", function() {
+db.once("open", () => {
 	console.log("Connected");
 });
 
 
 /***
-	Including controller
-***/
-var WordController = require("./WordController");
-
-
-/***
 	Adding body-parser middleware
 ***/
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-/***
-	Allowing CORs
-***/
-var allowCrossDomain = function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Methods", "GET,POST");
-	res.header("Access-Control-Allow-Headers", "Content-Type");
-
-	next();
-};
-app.use(allowCrossDomain);
 
 
 /***
 	Setting up routes
 ***/
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
 	res.send("Hello");
 });
-app.get("/getword", WordController.getword);
-app.post("/addword", WordController.addword);
-app.post("/flagword", WordController.flagword);
+
+app.get("/api/getsubject", (req, res) => {
+	SubjectController.getRandomSubject((msg) => {
+			return res.json(msg);
+	});
+});
+
+app.post("/api/addsubject", (req, res) => {
+	SubjectController.addSubject(req.body, (msg) => {
+		return res.json(msg);
+	});
+});
+
+app.post("/api/flagsubject", (req, res) => {
+	SubjectController.flagSubject(req.body, (msg) => {
+		return res.json(msg);
+	});
+});
 
 
 /***
